0faf715d93ade948cba3e1f350f046f8573b557f
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-05-25 10:43:41 +0530

    added the count on the cart item, moved the cart functions to cart utility file

--- src/components/CartIconComponent.jsx
+++ src/components/CartIconComponent.jsx
@@ -1,17 +1,20 @@
 import React, { useContext } from "react";
 import { ReactComponent as ShoppingIcon } from "../assets/shopping-bag.svg";
 import { GlobalContext } from "../context/GlobalState";
+import { selectCartItemsCount } from "../utils/cartUtils";
 
 const CartIconComponent = () => {
-  const { toggleCartHidden } = useContext(GlobalContext);
+  const { toggleCartHidden, cartItems } = useContext(GlobalContext);
 
   return (
     <div
-      className="flex justify-center align-top w-20 h-20 bold cursor-pointer"
+      className="flex justify-center align-top w-10 h-10 bold cursor-pointer"
       onClick={toggleCartHidden}
     >
-      <ShoppingIcon className="w-6" />
-      <span className="absolute mt-8 pt-1 text-xs font-bold">10</span>
+      <ShoppingIcon className="w-8 h-8" />
+      <span className="absolute mt-2 pt-1 text-xs font-bold">
+        {selectCartItemsCount(cartItems)}
+      </span>
     </div>
   );
 };
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,3 +1,5 @@
+import { addItemToCart, removeItemFromCart } from "../utils/cartUtils";
+
 export default (state, action) => {
   switch (action.type) {
     case "TOGGLE_CART_HIDDEN":
@@ -26,34 +28,4 @@ export default (state, action) => {
     default:
       return state;
   }
-};
-
-export const addItemToCart = (cartItems, cartItemToAdd) => {
-  const existingCartItem = cartItems.find(
-    (cartItem) => cartItem.id === cartItemToAdd.id
-  );
-
-  if (existingCartItem) {
-    return cartItems.map((cartItem) =>
-      cartItem.id === cartItemToAdd.id
-        ? { ...cartItem, quantity: cartItem.quantity + 1 }
-        : cartItem
-    );
-  }
-  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
-};
-
-export const removeItemFromCart = (cartItems, cartItemToRemove) => {
-  const existingCartItem = cartItems.find(
-    (cartItem) => cartItem.id === cartItemToRemove.id
-  );
-
-  if (existingCartItem.quantity === 1) {
-    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
-  }
-  return cartItems.map((cartItem) =>
-    cartItem.id === cartItemToRemove.id
-      ? { ...cartItem, quantity: cartItem.quantity - 1 }
-      : cartItem
-  );
 };
