8bca19fcbd6cd820f4c0c583d343d0022df34a8c
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-05-26 11:49:34 +0530

    modifying the file structure

--- src/components/CollectionPreview.jsx
+++ src/components/CollectionPreview.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import CollectionItem from "../components/CollectionItem";
 
 const CollectionPreview = ({ category }) => {
   return (
     <section>
       <h2 className="text-2xl m-3">{category.title}</h2>
-      <div className="flex flex-column"></div>
+      <div className="flex flex-column">
+        <div className="grid grid-cols-4">
+          {category.items
+            .filter((item, index) => index < 4)
+            .map((item) => (
+              <CollectionItem item={item} key={item.id} />
+            ))}
+        </div>
+      </div>
     </section>
   );
 };
